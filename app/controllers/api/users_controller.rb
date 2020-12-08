a4f72c22d7ca9a3deab0a810ee9b1fae2c313b19
Jae Doherty <jaeld97@gmail.com>
2020-12-08 14:29:35 -0800

    Finished Session Form

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,15 +3,16 @@ class Api::UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-        login(@user)
-        render :new
+            login(@user)
+            render :new
         else
-        render json: @user.errors.full_messages, status: 422
+            render json: @user.errors.full_messages, status: 422
         end
     end
 
     def destroy
         @user = User.find(params[:id])
+        logout
         render :new 
     end
 
--- frontend/matchstick.jsx
+++ frontend/matchstick.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/store';
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById('root');
-    const store = configureStore();
-
-    // testing - don't forget to delete
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+    // TESTING START
     window.getState = store.getState;
     window.dispatch = store.dispatch;
+    // TESTING END
 
-    ReactDOM.render(<h1>Welcome to MatchStick!</h1>, root);
-})
+    const root = document.getElementById("root");
+    ReactDOM.render(<Root store={store} />, root);
+});
