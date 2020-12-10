d7bd14f87901d1194dace0978e45b56e6d3ad683
Jae Doherty <jaeld97@gmail.com>
2020-12-09 19:08:10 -0800

    Finished splash greeting styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -11,7 +11,6 @@ const App = () => (
         <Modal />
         <header>
             <Link to="/" className="header-link"></Link>
-            <h1>MatchStick</h1>
             <GreetingContainer />
         </header>
         {/* <Switch>
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -7,15 +7,18 @@ const Greeting = ({ currentUser, logout, openModal }) => {
     const sessionLinks = () => (
         <div className="login-signup">
         <nav className="login">
-            <button onClick={() => openModal('login')}>LOG IN</button>
+        <h1 className="ms_head">MatchStick</h1>
+            <button className="li-button"onClick={() => openModal('login')}>LOG IN</button>
         </nav>
         <nav className="sign-up">
-            <button onClick={() => openModal('signup')}>SIGN UP</button>
+            <button className="su-button" onClick={() => openModal('signup')}>SIGN UP</button>
         </nav>
         </div>
         );
     const personalGreeting = () => (
+        
         <hgroup className="header-group">
+            <h1>MatchStick</h1>
             <h2 className="header-name">Hi, {currentUser.username}!</h2>
             <button className="header-button" onClick={logout}>Log Out</button>
         </hgroup>
