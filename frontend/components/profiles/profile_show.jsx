e8a129f7ed831a4c7af148cf593795adf113cfab
Jae Doherty <jaeld97@gmail.com>
2020-12-12 20:39:46 -0800

    Fixed info bar spacing

--- frontend/components/profiles/profile_show.jsx
+++ frontend/components/profiles/profile_show.jsx
@@ -21,26 +21,39 @@ class ProfileShow extends React.Component {
                 {/* <Link className="homeLink" to='/'>
                     <h1 className="home-head">MatchStick</h1>
                 </Link> */}
+                <div id="info-bar">
+                    <div>Photo Placeholder</div>
+                    <div id="name-age">
+                        <p id="name">{this.props.profile.first_name}</p>
+                        <div id="age-location">
+                            <p className="al">Age</p>
+                            <p className="al">{this.props.profile.zip_code}</p>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
                 <div id="profilewrapper">
                     <div className="profbio" id="bio"> About Me
                         <div id="biotext">
 
-                            <li>{this.props.profile.description}</li>
+                            <p>{this.props.profile.description}</p>
                         </div>
                     </div>
                     <div className="profile">
-
-                        <div className="profshow" id="name">Name
-                            <li>{this.props.profile.first_name}</li>
+                        <div className="profshow" id="edit-profile">
+                            <p>COMPLETE YOUR PROFILE<br/>and be seen by more people.</p>
                         </div>
-                        <div className="profshow" id="gender">Gender 
-                        <li>{this.props.profile.gender}</li>
+                        <div className="profshow" id="gender"> 
+                        <p>{this.props.profile.gender}</p>
                         </div>
-                        <div className="profshow" id="matchPrefs">Match Preferences
-                            <li>{this.props.profile.match_preferences}</li>
+                        <div className="profshow" id="matchPrefs">
+                            <p>{this.props.profile.match_preferences}</p>
                         </div>
                         <div className="profshow" id="quiz">
-                            <li>{this.props.profile.quiz_results}</li>
+                            <p>{this.props.profile.quiz_results}</p>
                         </div>
                     </div>
 
